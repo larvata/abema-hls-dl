1e2a7df2efd14e5f725407e536251f980d4d52e0
Larvata <alzzlnewtype@gmail.com>
2017-03-28 21:51:27 +0800

    finish poc

--- index.js
+++ index.js
@@ -3,63 +3,25 @@ const {
   getUserDetailsPromise,
   registUserDevicePromise,
   getMediaTokenPromise,
-  getMediaDetailsPromise,
-  // getLanceIdPromise,
-  getInitialPlaylistPromise,
+  scheduleDumpPromise,
 } = require('./lib/abema');
 
-
-
-
-
-
-
-const CustomXMLHttpRequest = require('./lib/customXMLHttpRequest');
-
-
-
-const req = new CustomXMLHttpRequest;
-console.log(req);
-
-const keyUrl = 'abematv://v2/abema-special/AeTZsRP1RgExDM/QDuH8HVpo4861hjkQ8HoY85';
-console.log('-----------------');
-req.open('qET', keyUrl);
-req.onload = function() {
-  console.log('reqed:', req.requestText);
+const scheduleOptions = {
+  channelId: 'abema-news',
+  recordDuration: 1,
 };
+const d = new Date();
+scheduleOptions.recordStart = d;
 
-console.log('%%%%%%%%%%%%%');
-req.send();
-
-
-
-
-
-
-
-
-
-process.exit();
 const deviceInfo = createUserDevice();
-console.log(deviceInfo);
-
-const dateRange = {
-  dateFrom: '20170317',
-  dateTo: '20170317',
-};
-
 const {deviceId, applicationKeySecret} = deviceInfo;
 registUserDevicePromise(deviceId, applicationKeySecret)
   .then(getUserDetailsPromise)
   .then(getMediaTokenPromise)
-  .then(getMediaDetailsPromise.bind(null, dateRange))
-  // .then(getLanceId)
-  .then(getInitialPlaylistPromise.bind(null, 'abema-news'))
+  .then(scheduleDumpPromise.bind(null, scheduleOptions))
   .then((result) => {
     console.log('final result:', result);
-    // require('fs').writeFileSync('channel.json', JSON.stringify(result, null, 2));
   })
   .catch((err) =>{
     console.log(err);
   });
-
